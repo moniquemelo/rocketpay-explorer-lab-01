41544ff667404e1a3774cee42a8a9f96752a7012
Monique Melo <71858557+moniquemelo@users.noreply.github.com>
2023-01-26 00:35:30 -0300

    Adição das referencias dos inputs

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ const ccGgColor01 = document.querySelector('.cc-bg svg > g g:nth-child(1) path')
 const ccGgColor02 = document.querySelector('.cc-bg svg > g g:nth-child(2) path')
 const ccLogo = document.querySelector('.cc-logo span:nth-child(2) img')
 
-// Função para mudar a cor do cartão de acordo com a bandeira
+//======== Função para mudar a cor do cartão de acordo com a bandeira ========//
 function setCardType(type) {
   const colors = {
     visa: ['#436D99', '#2D57F2'],
@@ -22,6 +22,7 @@ function setCardType(type) {
 
 globalThis.setCardType = setCardType
 
+//======== Criação das máscaras para CVC, Expiração e Número do cartão ========//
 const securityCode = document.querySelector('#security-code')
 const securityCodePattern = {
   mask: '0000'
@@ -51,7 +52,7 @@ const cardNumberPattern = {
   mask: [
     {
       mask: '0000 0000 0000 0000',
-      regex: /^4\d{0, 15}/,
+      regex: /^4\d{0,15}/,
       cardtype: 'visa'
     },
     {
@@ -73,5 +74,51 @@ const cardNumberPattern = {
     return foundMask
   }
 }
-
 const cardNumberMasked = IMask(cardNumber, cardNumberPattern)
+
+const cardHolder = document.querySelector('#card-holder')
+cardHolder.addEventListener('input', () => {
+  const ccHolder = document.querySelector('.cc-holder .value')
+  ccHolder.innerText = cardHolder.value.length === 0 ? 'FULANO DA SILVA' : cardHolder.value
+})
+
+const addButton = document.querySelector('#add-card')
+addButton.addEventListener('click', event => {
+  alert('Cartão adicionado!')
+})
+
+document.querySelector('form').addEventListener('submit', event => {
+  event.preventDefault()
+})
+
+// Referencia do input pro CVC
+securityCodeMasked.on('accept', () => {
+  updateSecurityCode(securityCodeMasked.value)
+})
+
+function updateSecurityCode(code) {
+  const ccSecurity = document.querySelector('.cc-security .value')
+  ccSecurity.innerText = code.length === 0 ? '123' : code
+}
+
+// Referencia do input pro número do cartão
+cardNumberMasked.on('accept', () => {
+  const cardType = cardNumberMasked.masked.currentMask.cardtype
+  setCardType(cardType)
+  updateCardNumber(cardNumberMasked.value)
+})
+
+function updateCardNumber(number) {
+  const ccNumber = document.querySelector('.cc-number')
+  ccNumber.innerText = number.length === 0 ? '1234 5678 9012 3456' : number
+}
+
+// Referencia do input pra data de expiração
+expirationDateMasked.on('accept', () => {
+  updateExpirationDate(expirationDateMasked.value)
+})
+
+function updateExpirationDate(date) {
+  const ccExpiration = document.querySelector('.cc-extra .value')
+  ccExpiration.innerText = date.length === 0 ? '02/32' : date
+}
